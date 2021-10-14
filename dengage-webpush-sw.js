a4934225c746de894d5b348973fccc0e1566e545
Sergen Tufanoğulları <sergen@whitehorse.technology>
2021-10-14 19:44:02 +0300

    feat: web push

--- dengage-webpush-sw.js
+++ dengage-webpush-sw.js
@@ -1,8 +1,7 @@
-
 var swUrl = new URL(location);
 var accountId = swUrl.searchParams.get('account_id') || '';
 var appGuid = swUrl.searchParams.get('app_guid') || '';
+var domain = swUrl.searchParams.get('domain') || '';
 if (accountId && appGuid) {
-    importScripts("https://op-pub.dengage.com/p/push/" + accountId + "/" + appGuid + "/dengage_sw.js");
+    importScripts("https://" + domain + "/p/push/" + accountId + "/" + appGuid + "/dengage_sw.js");
 }
-        
